659135b1cd01375c5f447d2059a6f3e324186143
ppopov-png <ppopov@trigonum.ae>
2026-08-31 14:29:22 +0300

    Wire multilingual site provider and deployment base path

--- apps/site/src/main.tsx
+++ apps/site/src/main.tsx
@@ -8,11 +8,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './app/App'
+import { I18nProvider } from './i18n/I18nProvider'
+
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '') || '/'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <I18nProvider>
+      <BrowserRouter basename={basename}>
+        <App />
+      </BrowserRouter>
+    </I18nProvider>
   </StrictMode>,
 )
