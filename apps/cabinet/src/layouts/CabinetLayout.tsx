5ac66e86869f98ffae83cfedb6e520d5918853b8
ppopov-png <ppopov@trigonum.ae>
2026-09-03 14:35:19 +0300

    Provide shared funding state

--- apps/cabinet/src/layouts/CabinetLayout.tsx
+++ apps/cabinet/src/layouts/CabinetLayout.tsx
@@ -1,20 +1,23 @@
 import { Outlet } from 'react-router-dom'
 import { BrokerAccountProvider } from '../shared/lib/AccountContext'
+import { FundingProvider } from '../shared/lib/FundingContext'
 import { Sidebar } from './Sidebar'
 import { Topbar } from './Topbar'
 
 export function CabinetLayout() {
   return (
     <BrokerAccountProvider>
-      <div className="flex min-h-screen bg-[var(--trigonum-bg)]">
-        <Sidebar />
-        <div className="flex min-w-0 flex-1 flex-col">
-          <Topbar />
-          <main className="flex-1 px-8 py-7">
-            <Outlet />
-          </main>
+      <FundingProvider>
+        <div className="flex min-h-screen bg-[var(--trigonum-bg)]">
+          <Sidebar />
+          <div className="flex min-w-0 flex-1 flex-col">
+            <Topbar />
+            <main className="flex-1 px-8 py-7">
+              <Outlet />
+            </main>
+          </div>
         </div>
-      </div>
+      </FundingProvider>
     </BrokerAccountProvider>
   )
 }
