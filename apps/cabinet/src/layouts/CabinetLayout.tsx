976048b96fbd44d96a8869f35bef8155e771a12c
ppopov-png <ppopov@trigonum.ae>
2026-09-03 13:55:29 +0300

    Wire account context into cabinet

--- apps/cabinet/src/layouts/CabinetLayout.tsx
+++ apps/cabinet/src/layouts/CabinetLayout.tsx
@@ -1,17 +1,20 @@
 import { Outlet } from 'react-router-dom'
+import { BrokerAccountProvider } from '../shared/lib/AccountContext'
 import { Sidebar } from './Sidebar'
 import { Topbar } from './Topbar'
 
 export function CabinetLayout() {
   return (
-    <div className="flex min-h-screen bg-[var(--trigonum-bg)]">
-      <Sidebar />
-      <div className="flex min-w-0 flex-1 flex-col">
-        <Topbar />
-        <main className="flex-1 px-8 py-7">
-          <Outlet />
-        </main>
+    <BrokerAccountProvider>
+      <div className="flex min-h-screen bg-[var(--trigonum-bg)]">
+        <Sidebar />
+        <div className="flex min-w-0 flex-1 flex-col">
+          <Topbar />
+          <main className="flex-1 px-8 py-7">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </BrokerAccountProvider>
   )
 }
